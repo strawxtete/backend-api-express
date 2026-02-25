0760335224b815d4d925aa254af0aa5c1c045f2d
sthefany <sthefany.santiago@aluno.ifsp.edu.br>
2026-02-24 22:07:14 -0300

    HTTP methods sample.

--- src/server.js
+++ src/server.js
@@ -5,9 +5,31 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Olá Mundo Express API!')
+})
+
+// CRUD - Create, Read, Update, Delete
+
+app.get('/user', (req, res) => {
+  res.send('Exemplo de GET na rota /user')
+})
+
+app.post('/user', (req, res) => {
+  res.send('Exemplo de POST na rota /user')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Exemplo de PUT na rota /user')
+})
+
+app.patch('/user', (req, res) => {
+  res.send('Exemplo de PATCH na rota /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Exemplo de DELETE na rota /user')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Servidor rodando em http://localhost:${port}`)
 })
